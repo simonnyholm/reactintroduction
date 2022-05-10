47dc999b98fcfffa33926aa9dfc970e95e2f2a03
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-10 08:29:40 +0200

    update according to net ninja lesson 4

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Den bloggede blog</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
