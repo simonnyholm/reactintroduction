c7939a734f69b4d59aa610e444c7fe5fd3e65ac2
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-18 09:58:12 +0200

    add useEffect

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,20 +3,35 @@ import BlogList from "./blogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect kørte");
-    fetch("http://localhost:8000/blogs").then((res) => {
-      return res.json().then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
-    });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Data fra serveren er ikke tilgængelig");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setIsPending(false);
+          setBlogs(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1200);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Blogindlæg indlæses...</div>}
       {blogs && <BlogList blogs={blogs} title="Alle blogindlæg" />}
+      {error && <div>{error}</div>}
     </div>
   );
 };
